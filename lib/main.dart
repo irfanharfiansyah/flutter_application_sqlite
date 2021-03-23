10ca60f86f61011977bb80dc820c8834da92d887
irfanharfiansyah <Irfanharfiansyah37@gmail.com>
2021-03-23 13:41:40 +0700

    Call the home class on Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_sqlite/pages/home.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp  extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Tambahkan Item",
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
+      home: Home(),
     );
   }
 }
