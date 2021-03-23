db1751a4bb3f220274977032efa80cf39da1063e
irfanharfiansyah <Irfanharfiansyah37@gmail.com>
2021-03-23 14:15:43 +0700

    Add setter getter for stok and kode

--- lib/models/item.dart
+++ lib/models/item.dart
@@ -3,6 +3,16 @@ class Item {
   int _id;
   String _name;
   int _price;
+  int _qty;
+  int _kode;
+
+ get qty => this._qty;
+
+ set qty(value) => this._qty = qty;
+
+ get kode => this._kode;
+
+ set kode(value) => this._kode = kode;
 
 int get id => _id;
 
@@ -13,13 +23,15 @@ get price => this._price;
 set price( value) => this._price = value;
 
  //konstruktor versi 1
- Item(this._name, this._price);
+ Item(this._name, this._price, this._qty, this._kode);
 
 // //konstruktor versi 2: konversi dari Map ke Item
  Item.fromMap(Map<String, dynamic> map){
    this._id = map['id'];
    this._name = map['name'];
    this._price = map['price'];
+   this._qty = map['qty'];
+   this._kode = map['kode'];
  } 
 
 // //konversi dari Item ke Map
@@ -28,6 +40,8 @@ set price( value) => this._price = value;
    map['id'] = this._id;
    map['name'] = name;
    map['price'] = price;
+   map['qty'] = qty;
+   map['kode'] = kode;
    return map;
  }
 }
