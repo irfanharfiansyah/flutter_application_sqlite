125eb882940a3314d298bea601b0a364241107b3
irfanharfiansyah <Irfanharfiansyah37@gmail.com>
2021-03-24 08:51:59 +0700

    Add theme color and fonts

---
+++ lib/theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'dart:ui';
+
+// Warna
+Color blue1 = Color(0xFF6200EE);
+Color black1 = Colors.black.withOpacity(0.6);
+Color green1 = Colors.teal[200];
+// Fonts
+TextStyle title = GoogleFonts.poppins();
+TextStyle body = GoogleFonts.montserrat();
+TextStyle but = GoogleFonts.roboto();
